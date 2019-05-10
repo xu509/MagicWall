5148eda5c83380e0c6ce197830b8ca88e4dae87f
xuningxin <xu509@outlook.com>
2019-05-10 17:29:41 +0800

    123
    111

--- Assets/Script/Dao/pojo/Activity.cs
+++ Assets/Script/Dao/pojo/Activity.cs
@@ -36,8 +36,11 @@ public class Activity : Generator<Activity>
         Activity activity = new Activity();
         activity.ent_id = 1;
 
-        string[] images = {"1.jpg","2.png","3.png","4.png","5.png"
-                ,"6.png","7.png","8.png"};
+        string[] images = {"1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg","9.jpg","10.jpg",
+            "11.jpg","12.jpg","13.jpg","14.jpg","15.jpg","16.jpg","17.jpg","18.jpg","19.jpg","20.jpg",
+            "21.jpg","22.jpg","23.jpg","24.jpg","25.jpg","26.jpg","27.jpg","28.jpg","29.jpg","30.jpg",
+            "31.jpg","32.jpg","33.jpg","34.jpg","35.jpg"
+        };
         activity._image = images[Random.Range(0, images.Length - 1)];
 
         string[] names = { "2018年春夏巴黎时装秀", "巴黎时装周（Paris Fashion Week）1910年，由法国时装协会主办。", "在米兰和伦敦的时装周相当保守，它们更喜欢本土的设计"};
