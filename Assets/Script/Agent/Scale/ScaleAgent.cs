5f7afcddd5e7c8a67c462902847959fe89d5fb5d
zhangxing <873074332@qq.com>
2019-05-24 15:54:48 +0800

    111

--- Assets/Script/Agent/Scale/ScaleAgent.cs
+++ Assets/Script/Agent/Scale/ScaleAgent.cs
@@ -4,13 +4,16 @@ using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
-public class ScaleAgent : MonoBehaviour
+
+public class ScaleAgent : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
     Texture _imageTexture;
 
     [SerializeField] RawImage image;
     [SerializeField] RectTransform tool_box;
 
+    public float maxScale = 2.0f;//最大倍数
+    public int plusCount = 5;//放大次数
 
     float MAX_WIDTH = 660;
     float MAX_HEIGHT = 950;
@@ -18,22 +21,34 @@ public class ScaleAgent : MonoBehaviour
     Action OnCloseClicked;
     Action _onReturnClicked;
 
+    private RectTransform imgRtf;
+    private float currentScale;//当前缩放倍数
+    private float perScale;//每次放大倍数
+    private Vector2 originalSize;
 
+    //拖动
+    private Vector2 first = Vector2.zero;
+    private Vector2 second = Vector2.zero;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //print(imgRtf.localPosition);
     }
 
     public void SetImage(Texture texture)
     {
+
+        imgRtf = image.GetComponent<RectTransform>();
+        currentScale = 1;
+        perScale = (maxScale - 1) / plusCount;
+
         // 需要防止变形
         _imageTexture = texture;
 
@@ -66,16 +81,32 @@ public class ScaleAgent : MonoBehaviour
     public void DoPlus()
     {
         // TODO 放大图片操作
-        Debug.Log("放大图片操作");
+        //Debug.Log("放大图片操作");
+        if (currentScale < maxScale)
+        {
+            currentScale += perScale;
+            ResetImage();
+        }
+        
     }
 
     // 点击减少按钮
     public void DoMinus()
     {
         // TODO 缩小 图片操作
-        Debug.Log("缩小图片操作");
+        //Debug.Log("缩小图片操作");
+        if (currentScale > 1.0f)
+        {
+            currentScale -= perScale;
+            ResetImage();
+            imgRtf.anchoredPosition = Vector2.zero;
+        }
     }
 
+    private void ResetImage()
+    {
+        imgRtf.sizeDelta = new Vector2(originalSize.x * currentScale, originalSize.y * currentScale);
+    }
 
     private void SizeToScale()
     {
@@ -100,14 +131,48 @@ public class ScaleAgent : MonoBehaviour
             str = "WIDTH < MAX WIDTH";
         }
 
+        //设置图片原始大小
+        originalSize = new Vector2(w, h);
 
         // 将mask调整至新的大小
-        parent.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, w);
-        parent.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, h);
+        //parent.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, w);
+        //parent.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, h);
 
-        parent.parent.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, w);
-        parent.parent.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, h);
+        //parent.parent.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, w);
+        //parent.parent.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, h);
+
+        imgRtf.sizeDelta = originalSize;
+        parent.sizeDelta = originalSize;
+        parent.parent.GetComponent<RectTransform>().sizeDelta = originalSize;
+
 
 
     }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        first = eventData.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        second = eventData.position;
+        float x = imgRtf.anchoredPosition.x;
+        float y = imgRtf.anchoredPosition.y;
+        x += second.x - first.x;
+        y += second.y - first.y;
+        print("x:" + x + " y:" + y);
+        Vector2 currentSize = imgRtf.sizeDelta;
+        if (Mathf.Abs(x) <= (currentSize.x - originalSize.x)/2 && Mathf.Abs(y) <= (currentSize.y - originalSize.y)/2)
+        {
+            imgRtf.anchoredPosition = new Vector2(x, y);
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+
+    }
+
+
 }
