61d27a0855429077300973b2c459dedf7a37c0a5
xuningxin <xu509@outlook.com>
2019-06-04 17:34:13 +0800

    12313
    111

--- Assets/Script/Config/ManagerConfig.cs
+++ Assets/Script/Config/ManagerConfig.cs
@@ -4,5 +4,12 @@ using UnityEngine;
 
 public class ManagerConfig : ScriptableObject
 {
+    [Range(100f, 10000f), Header("星空效果，最远距离")]
+    public float StarEffectDistance;
+
+    [Range(2f, 10f), Header("星空效果，动画时间")]
+    public float StarEffectDistanceTime;
+
+
 
 }
