6f5f934f1edf8bd104bdc0af20bc808abb67d17c
xuningxin <xu509@outlook.com>
2019-06-04 18:13:08 +0800

    1231
    111

---
+++ Assets/Script/Search/SearchAgent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//
+//  搜索代理
+//
+public class SearchAgent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
