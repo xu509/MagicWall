d403819da01111ddcb9c1ed9dfa8ff35b5230ad5
xuningxin <xu509@outlook.com>
2019-06-19 14:26:18 +0800

    123
    1

--- Assets/Script/Search/SearchResult/SearchResultScrollBarItemAgent.cs
+++ Assets/Script/Search/SearchResult/SearchResultScrollBarItemAgent.cs
@@ -1,17 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class SearchResultScrollBarItemAgent : MonoBehaviour
 {
-    // 最短宽度20，最长宽度50
-
-
 
     private int _index;
-    private int _total;
+    private float _width;   // 图片宽度
+    private float _default_width;   // 默认宽度
 
 
+    [SerializeField] private Image _image;  //  图片
+
+    #region 引用
+    public int Index { get { return _index; } set { _index = value; } }
+    #endregion
 
     // Start is called before the first frame update
     void Start()
@@ -25,19 +30,38 @@ public class SearchResultScrollBarItemAgent : MonoBehaviour
         
     }
 
-    public void Init(int index) {
+    public void Init(int index,float _initItemWidth) {
         _index = index;
         //_total = total;
+        _default_width = _initItemWidth;
+        SetImageWidth(0);
     }
 
-    /// <summary>
-    /// 刷新状态
-    /// </summary>
-    /// <param name="position">1 -> 0</param>
-    public void Refresh(float position) {
 
+    public void Refresh(float widthOffset) {
 
-
+        if ((widthOffset + _default_width) != _width) {
+            SetImageWidth(widthOffset);
+        }
 
     }
+
+    private void SetImageWidth(float widthOffset) {
+
+        // 只有一半进行变化
+        //_image.GetComponent<RectTransform>().sizeDelta = new Vector2(width, _image.GetComponent<RectTransform>().sizeDelta.y);
+        _width = widthOffset + _default_width;
+
+        _image.GetComponent<RectTransform>()
+            .DOSizeDelta(new Vector2(_width , _image.GetComponent<RectTransform>().sizeDelta.y), 0.2f)
+            .OnUpdate(() => {
+                // 根据宽度进行调整
+                float anchorx;
+                anchorx = (_image.GetComponent<RectTransform>().sizeDelta.x - _default_width) / 2;
+                anchorx = 0 - anchorx;
+                _image.GetComponent<RectTransform>().anchoredPosition = new Vector2(anchorx, _image.GetComponent<RectTransform>().anchoredPosition.y);
+            });
+
+    }
+
 }
