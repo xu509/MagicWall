dde8f5484330cde31fc8ed277cffcd7a943dce79
Alan <xu509@outlook.com>
2019-09-24 01:50:06 +0800

    11
    1

---
+++ Assets/Script/Agent/Data/FlockStatusEnum.cs
@@ -0,0 +1,13 @@
+﻿namespace MagicWall {
+
+    public enum FlockStatusEnum 
+    {
+        PREPARED,   // 准备中
+        RUNIN,      // 进场中
+        NORMAL,     // 正常
+        OBSOLETE,   // 废弃的
+        HIDE        // 隐藏中
+    }
+
+}
+
